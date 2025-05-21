7b601130b3eac3ace96dd049a69bb1153dfcb2ee
ikerguerra <152733086+ikerguerra@users.noreply.github.com>
2025-05-21 20:52:43 +0200

    Envio correos EmailJS

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,27 +1,82 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import toast, { Toaster } from 'react-hot-toast';
+
 
 export const Contacto = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        lastName: '',
+        email: '',
+        message: ''
+    });
+
+    const form = useRef();
+    
+    const { name, lastName, email, message } = formData;
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(process.env.REACT_APP_SERVICE, process.env.REACT_APP_TEMPLATE, form.current, {
+                publicKey: process.env.REACT_APP_PUBLIC_KEY,
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    toast.success("Email enviado correctamente");
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                    toast.error("Error al enviar el email. Inténtalo de nuevo");
+                },
+            );
+
+        setFormData({
+            name: '',
+            lastName: '',
+            email: '',
+            message: ''
+        });
+    };
+
     return (
-        <form>
-            <div className='d-flex row'>
-                <div className="mb-3 col-6">
-                    <label htmlFor="inputNombre" className="form-label">Nombre</label>
-                    <input type="text" className="form-control" id="inputNombre" placeholder='Nombre' />
+        <>
+            <form ref={form} onSubmit={sendEmail}>
+                <div className='d-flex row'>
+                    <div className="mb-3 col-6">
+                        <label htmlFor="inputNombre" className="form-label">Nombre</label>
+                        <input type="text" className="form-control" id="inputNombre" name='name' placeholder='Nombre' value={name} onChange={handleChange}/>
+                    </div>
+                    <div className="mb-3 col-6">
+                        <label htmlFor="inputApellido" className="form-label">Apellido</label>
+                        <input type="text" className="form-control" id="inputApellido" name='lastName' placeholder='Apellido' value={lastName} onChange={handleChange}/>
+                    </div>
                 </div>
-                <div className="mb-3 col-6">
-                    <label htmlFor="inputApellido" className="form-label">Apellido</label>
-                    <input type="text" className="form-control" id="inputApellido" placeholder='Apellido' />
+                <div className="mb-3">
+                    <label htmlFor="inputEmail" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="inputEmail" name='email' placeholder='email@email.com' value={email} onChange={handleChange}/>
                 </div>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="inputEmail" className="form-label">Email</label>
-                <input type="email" className="form-control" id="inputEmail" placeholder='email@email.com' />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="textAreaMensaje" className="form-label">Mensaje</label>
-                <textarea className='form-control' name="mensaje" id="textAreaMensaje" placeholder='Escribe tu mensaje' rows="5"></textarea>
-            </div>
-            <button type="submit" className="btn btn-primary">Enviar</button>
-        </form>
+                <div className="mb-3">
+                    <label htmlFor="textAreaMensaje" className="form-label">Mensaje</label>
+                    <textarea className='form-control' name='message' id="textAreaMensaje" placeholder='Escribe tu mensaje' rows="5" value={message} onChange={handleChange}></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Enviar</button>
+            </form>
+            
+            {/* Toast mensaje envio del correo */}
+            <Toaster
+                position="bottom-center"
+                reverseOrder={true}
+            />
+        </>
     )
 }
